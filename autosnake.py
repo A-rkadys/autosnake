702b47cdade961f4bcde6b81e7c3e7e5351b0183
aZrog5 <incentbrisset@gmail.com>
2023-10-17 00:50:34 +0200

    class snake + mouvement

--- autosnake.py
+++ autosnake.py
@@ -1,5 +1,3 @@
-#snake
-
 import pygame as py
 import random as r
 from pygame.locals import (
@@ -21,6 +19,26 @@ time = 0
 
 #snake
 class Player(py.sprite.Sprite):
-	def __init__(self):
-		super(Player, self).__init__()
-		self.surf = py.Suface((25, 25))
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = py.Suface((25, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
