c50cee8949119325dac186d1297ce064cbe1410e
A-rkadys <lilian.lhotel222@gmail.com>
2023-10-17 05:37:27 +0200

    Player is now on a grid

--- apple.py
+++ apple.py
@@ -1,15 +1,15 @@
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, applepicture
+from constants import (
+    SCREEN_WIDTH, SCREEN_HEIGHT,
+    applepicture,
+    MAX_X, MAX_Y,
+    PLAYER_WIDTH, PLAYER_HEIGHT
+)
 from math import floor
 from pygame import Surface, image
 from pygame.sprite import Sprite
 from random import randint
 
 class Pomme(Sprite):
-
-    DIM_X:int = 33
-    DIM_Y:int = 28
-    MAX_X:int = floor(SCREEN_WIDTH / DIM_X) - 1
-    MAX_Y:int = floor(SCREEN_HEIGHT / DIM_Y) - 1
 
     def __init__(self):
         super(Pomme, self).__init__() 
@@ -21,8 +21,8 @@ class Pomme(Sprite):
 
     def replace(self):
         self.rect.move_ip(
-            self.DIM_X * (randint(0, self.MAX_X) - (self.rect.left / self.DIM_X)),
-            self.DIM_Y * (randint(0, self.MAX_Y) - (self.rect.top / self.DIM_Y))
+            PLAYER_WIDTH * (randint(0, MAX_X) - (self.rect.left / PLAYER_WIDTH)),
+            PLAYER_WIDTH * (randint(0, MAX_Y) - (self.rect.top / PLAYER_HEIGHT))
         )
         print(f"x:{self.rect.left}\ny: {self.rect.top}\n")
 
--- player.py
+++ player.py
@@ -3,34 +3,34 @@ from pygame.sprite import Sprite
 from constants import (
     K_UP, K_DOWN, K_LEFT, K_RIGHT,
     SCREEN_WIDTH, SCREEN_HEIGHT,
+    PLAYER_WIDTH, PLAYER_HEIGHT,
+    MAX_X, MAX_Y,
     WHITE
 )
 
 class Player(Sprite):
 
-    DIM_X = 25
-    DIM_Y = 25
-    MAX_X = SCREEN_WIDTH - DIM_X
-    MAX_Y = SCREEN_HEIGHT - DIM_Y
-
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = Surface((self.DIM_X, self.DIM_Y))
+        self.surf = Surface((PLAYER_WIDTH, PLAYER_HEIGHT))
         self.surf.fill(WHITE)
         self.rect = self.surf.get_rect()
-        self.rect.move_ip(self.MAX_X >> 1, self.MAX_Y >> 1)
+        self.rect.move_ip(
+            SCREEN_WIDTH - ((SCREEN_WIDTH >> 1) % PLAYER_WIDTH),
+            SCREEN_HEIGHT - ((SCREEN_HEIGHT >> 1) % PLAYER_HEIGHT)
+        )
 
     def update(self, pressed_keys):
         self.rect.move_ip(
-            self.DIM_X * (pressed_keys[K_RIGHT] - pressed_keys[K_LEFT]),
-            self.DIM_Y * (pressed_keys[K_DOWN] - pressed_keys[K_UP])
+            PLAYER_WIDTH * (pressed_keys[K_RIGHT] - pressed_keys[K_LEFT]),
+            PLAYER_HEIGHT * (pressed_keys[K_DOWN] - pressed_keys[K_UP])
         )
         # To correct position if oob (horizontal axis)
-        self.rect.left *= (0 < self.rect.left and self.rect.left <= self.MAX_X)
-        self.rect.left += self.MAX_X * (self.rect.left > self.MAX_X)
-        self.rect.right = self.rect.left + self.DIM_X
+        self.rect.left *= (0 < self.rect.left and self.rect.left < SCREEN_WIDTH)
+        self.rect.left += SCREEN_WIDTH * (self.rect.left > SCREEN_WIDTH)
+        self.rect.right = self.rect.left + PLAYER_WIDTH
         # To correct position if oob (vertical axis)
-        self.rect.top *= (0 < self.rect.top and self.rect.top <= self.MAX_Y)
-        self.rect.top += (self.MAX_Y) * (self.rect.top > self.MAX_Y)
-        self.rect.bottom = self.rect.top + self.DIM_Y
+        self.rect.top *= (0 < self.rect.top and self.rect.top < SCREEN_HEIGHT)
+        self.rect.top += SCREEN_HEIGHT * (self.rect.top > SCREEN_HEIGHT)
+        self.rect.bottom = self.rect.top + PLAYER_HEIGHT
 
