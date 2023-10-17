a2b149bbf303fc3e6058394a55d96224d342b40d
A-rkadys <lilian.lhotel222@gmail.com>
2023-10-17 02:47:37 +0200

    Refactored main code into files: created player.py, constants.py and apple.py (still empty)

--- autosnake.py
+++ autosnake.py
@@ -1,54 +1,14 @@
 import pygame as py
 import random as r
-from pygame.locals import (
-    RLEACCEL,
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    KEYDOWN,
-    K_ESCAPE,
+from constants import (
+    SCREEN_WIDTH, SCREEN_HEIGHT,
+    K_UP, K_DOWN, K_LEFT, K_RIGHT,
+    KEYDOWN, K_ESCAPE,
     QUIT
 )
-
-white = (255, 255, 255)
-green = (0, 255, 0)
-blue = (0, 0, 128)
-red = (255, 0, 0)
-time = 0
-
-#snake
-class Player(py.sprite.Sprite):
-    def __init__(self):
-        super(Player, self).__init__()
-        self.surf = py.Surface((25, 25))
-        self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect()
-    def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
-
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
-
+from player import Player, Sprite
 
 py.init()
-
-#taille de l'écran
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 
 #creation de l'écran
 screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -56,8 +16,6 @@ ADDENEMY = py.USEREVENT +1
 py.time.set_timer(ADDENEMY, 250)
 
 player = Player()
-
-
 
 all_sprites = py.sprite.Group()
 all_sprites.add(player)
@@ -71,6 +29,7 @@ py.display.flip()
 running2 = False
 running = True
 count = 60
+time = 0
 while running:
     for event in py.event.get():
         if event.type == KEYDOWN:
@@ -78,11 +37,8 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
-
-
     pressed_keys = py.key.get_pressed()
     player.update(pressed_keys)
-
     # Fill the screen with black
     screen.fill((0, 0, 0))
     #count += 0.1
@@ -108,3 +64,4 @@ while running2:
 
     py.display.update()
     py.display.flip()
+
---
+++ constants.py
@@ -0,0 +1,19 @@
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    KEYDOWN,
+    K_ESCAPE,
+    QUIT
+)
+
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 128)
+RED = (255, 0, 0)
+
+#taille de l'écran
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
