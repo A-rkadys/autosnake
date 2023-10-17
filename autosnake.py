673cb9547f7577583ec2ba919a9f2a33f5dbc842
aZrog5 <incentbrisset@gmail.com>
2023-10-17 03:38:12 +0200

    O'Propre

--- autosnake.py
+++ autosnake.py
@@ -2,85 +2,10 @@ import pygame as py
 import sys as sys
 import os as os
 from constants import *
-from player import Player
-from apple import Pomme
+from gameloop import gameloop
 
 
-time = 0
-score = 0
 
 py.init()
 py.font.init()
-
-
-
-#creation de l'écran
-screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-ADDENEMY = py.USEREVENT +1
-py.time.set_timer(ADDENEMY, 250)
-
-player = Player()
-pomme = Pomme()
-
-
-all_sprites = py.sprite.Group()
-all_sprites.add(player)
-
-clock = py.time.Clock()
-
-#screen refresh
-py.display.flip()
-
-#boucle infinie
-running2 = False
-running = True
-font = py.font.Font(None, 36)
-count = 60
-while running:
-    for event in py.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
-        elif event.type == QUIT:
-            running = False
-
-
-    pressed_keys = py.key.get_pressed()
-    player.update(pressed_keys)
-
-    # Fill the screen with black
-    screen.fill((0, 0, 0))
-    time += 1
-    for entity in all_sprites:
-        screen.blit(entity.surf, entity.rect)
-        py.draw.rect(screen, (255   , 0, 0), pomme)
-
-    score_text = font.render(f'Score: {score}', True, (0, 255, 0))
-    screen.blit(score_text, (10, 10))
-    
-    # Update the display
-    py.display.flip()
-    clock.tick(count)
-    if player.rect.colliderect(pomme.rect):
-        score += score_increment
-        pomme.replace()
-
-    """if player.rect.colliderect(,):
-        player.kill()"""
-
-    
-
-while running2:
-    for event in py.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running2 = False
-        elif event.type == QUIT:
-            running2 = False
-        
-    screen_score.fill((0, 0, 0))
-    screen_score.blit(text, textRect)
-    screen_score.blit(text2, textRect2)
-
-    py.display.update()
-    py.display.flip()
+gameloop()
---
+++ gameloop.py
@@ -0,0 +1,45 @@
+import pygame as py
+from constants import *
+from player import Player
+from apple import Pomme
+
+def gameloop():
+	screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	py.display.flip()
+	player = Player()
+	apple = Pomme()
+	all_sprites = py.sprite.Group()
+	all_sprites.add(player)
+	clock = py.time.Clock()
+	FONT = py.font.Font(None, 36)
+	running = True
+	time = 0
+	score = 0
+	while running:
+	    for event in py.event.get():
+	        if event.type == KEYDOWN:
+	            if event.key == K_ESCAPE:
+	                running = False
+	        elif event.type == QUIT:
+	            running = False
+
+
+	    pressed_keys = py.key.get_pressed()
+	    player.update(pressed_keys)
+
+	    # Fill the screen with black
+	    screen.fill((0, 0, 0))
+	    time += 1
+	    for entity in all_sprites:
+	        screen.blit(entity.surf, entity.rect)
+	        py.draw.rect(screen, (255   , 0, 0), apple)
+
+	    score_text = FONT.render(f'Score: {score}', True, (0, 255, 0))
+	    screen.blit(score_text, (10, 10))
+	    
+	    # Update the display
+	    py.display.flip()
+	    clock.tick(60)
+	    if player.rect.colliderect(apple.rect):
+	        score += score_increment
+	        apple.replace()
