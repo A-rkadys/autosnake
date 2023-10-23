37d9aca192ed998df0a2a1531ea920a7f09a402b
A-rkadys <lilian.lhotel222@gmail.com>
2023-10-23 15:50:25 +0200

    Refactor of gameloop: Created Game class to wrap the main game process

--- sources/gameloop.py
+++ sources/gameloop.py
@@ -1,70 +1,38 @@
 from random import randint
 from sources.constants import (
-    SCREEN_WIDTH, SCREEN_HEIGHT,
     MUSICS,
-    SCORE_INCREMENT,
-    BLACK,
-    FONT,
-    BODY_SURFACE,
     CLOCK_TICK,
-    QUIT, 
-    K_ESCAPE,
-    OOF
+    QUIT, K_ESCAPE,
 )
-import pygame as py
-from sources.player import Player
-from sources.apple import Pomme
+from pygame import (
+    display,
+    event,
+    key,
+    mixer
+)
+from sources.game import Game
 
 def gameloop():
-    screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    py.display.flip()
-    player = Player()
-    apple = Pomme()
-    all_sprites = py.sprite.Group()
-    all_sprites.add(player)
-    clock = py.time.Clock()
-    running = True
-    time = 0
-    score = 0
-
-
-    while running:
-        if (not py.mixer.get_busy()):
+    game = Game(display)
+    while True:
+        if (not mixer.get_busy()):
             MUSICS[randint(0, MUSICS.__len__() - 1)].play()
-
-        for event in py.event.get():
-            if event.type == QUIT:
-                running = False; break
-
-        pressed_keys = py.key.get_pressed()
+        # Event handling
+        for e in event.get():
+            if e.type == QUIT:
+                return
+        # Check for escape
+        pressed_keys = key.get_pressed()
         if (pressed_keys[K_ESCAPE]):
-            running = False; break
-        
-        player.update(pressed_keys)
-
-        if not player.alive:
             return
-
-        if player.rect.colliderect(apple.rect):
-            OOF.play()
-            score += SCORE_INCREMENT
-            apple.replace()
-            player.growth()
-           
-
-        screen.fill(BLACK)
-        screen.blit(apple.image, apple.rect)
-
-        i = player.l_body - 1
-        while (i):
-        	screen.blit(BODY_SURFACE, player.body[i]); i -= 1
-        screen.blit(player.surf, player.rect)
-
-        score_text = FONT.render(f'Score: {score}', True, (0, 255, 0))
-        screen.blit(score_text, (10, 10))
-        screen.blit(apple.image, apple.rect)
-
-        # Update the display
-        py.display.flip()
-        clock.tick(CLOCK_TICK)
+        # Update
+        game.update(pressed_keys)
+        # End game if player died
+        if not game.player.alive:
+            return
+        # Display game screen
+        game.blit_all()
+        game.display.flip()
+        # Ticks
+        game.clock.tick(CLOCK_TICK)
 
